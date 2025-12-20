d3f1618dc417ced5de6a8512feec847f6399d62c
williamdev20 <williamalvs45@gmail.com>
2025-12-20 14:39:54 -0300

    fix: Finalizar tela de login do mobile

--- mobile/MiopeMais/app/screens/login/login_screen.jsx
+++ mobile/MiopeMais/app/screens/login/login_screen.jsx
@@ -1,43 +1,68 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { styles } from "./style";
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const checkCredentials = () => {
-        
+        if (email.trim() === "" && password.trim() === "") {
+            alert("Campos faltando!")
+        } else {
+            navigation.navigate("Signup")
+        }
     }
 
     return (
         <View style={styles.container}>
-            <View style={styles.formContainer}>
-                <Text style={styles.label}>Email</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Digite seu email aqui"
-                    value={email}
-                    onChangeText={setEmail}
-                    keyboardType="email-address"
-                />
-                <Text style={styles.label}>Senha</Text>
-                <TextInput 
-                    style={styles.input}
-                    placeholder="Digite sua senha aqui"
-                    value={password}
-                    onChangeText={setPassword}
-                    keyboardType="default"
-                    secureTextEntry={true}
-                />
-                <TouchableOpacity
-                style={styles.button}
-                >
-                    <Text style={styles.textButton}>Entrar</Text>
-                </TouchableOpacity>
-            </View>
+            <View style={styles.subContainer}>
+                <Image style={styles.logo} source={require("../../../assets/logo/miope-mais-logo.png")}/>
 
-            {/*<Text>Olá, {email}</Text>*/}
+                <View style={styles.formContainer}>
+                    <Text style={styles.label}>Email</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Digite seu email aqui"
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                    />
+                    <Text style={styles.label}>Senha</Text>
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="Digite sua senha aqui"
+                        value={password}
+                        onChangeText={setPassword}
+                        keyboardType="default"
+                        secureTextEntry={true}
+                    />
+
+                    <View style={styles.registerContainer}>
+                        <Text>Não tem uma conta? </Text><Text
+                            style={styles.goSignUp}
+                            onPress={() => navigation.navigate("Signup")}>
+                        Crie agora!
+                        </Text>
+                    </View>
+
+                    <TouchableOpacity
+                    style={styles.button}
+                    onPress={checkCredentials}
+                    >
+                        <Text style={styles.textButton}>Entrar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                    style={styles.googleButton}
+                    >
+                        <View style={styles.googleContainer}>
+                            <Image style={styles.googleImage} source={require("../../../assets/login/google.png")} />
+                            <Text style={styles.googleTextButton}>Continuar com Google</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 }
--- mobile/MiopeMais/app/screens/login/style.jsx
+++ mobile/MiopeMais/app/screens/login/style.jsx
@@ -5,11 +5,24 @@ export const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
+        width: "100%",
+        backgroundColor: "#f5f5f5"
+    },
+
+    subContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 40,
         width: "100%"
+    },
+    
+    logo: {
+        width: 150,
+        height: 150,
+        
     },
 
     formContainer: {
-        flex: 1,
         alignItems: "center",
         justifyContent: "center",
         width: "100%",
@@ -17,25 +30,64 @@ export const styles = StyleSheet.create({
     },
 
     label: {
-        justifyContent: "flex-start"
+        alignSelf: "flex-start",
+        marginLeft: "8%",
+        fontSize: 15
     },
 
     input: {
-        width: "80%",
+        width: "90%",
         borderWidth: 1,
         borderColor: "#999",
-        width: "80%"
+        height: 60,
+        borderRadius: 10,
+        paddingHorizontal: 15
+    },
+
+    registerContainer: {
+        flexDirection: "row"
+    },
+
+    goSignUp: {
+        color: "#00ccffff"
     },
 
     button: {
         backgroundColor: "brown",
-        width: "80%",
-        height: "5%",
+        width: "90%",
+        height: 50,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        marginTop: 15,
+        borderRadius: 10
     },
 
      textButton: {
-        color: "#fff"  
-    }
+        color: "#fff",
+        fontSize: 17
+    },
+
+    googleContainer: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 15
+    },
+
+    googleButton: {
+        backgroundColor: "#fff",
+        width: "90%",
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#000"
+    },
+
+    googleImage: {
+        width: 30,
+        height: 30
+    },
 });
